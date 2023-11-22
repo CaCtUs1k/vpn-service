a8ebcb414f5044860f5fe473e8dcc92ae1c95375
CaCtUs1k <denischernish19012004@gmail.com>
2023-11-22 19:16:01 +0200

    Provide models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Site(models.Model):
+    user = models.ForeignKey(User, related_name="sites", on_delete=models.CASCADE)
+    name = models.CharField(max_length=63, unique=True)
+    url = models.URLField()
+
+
+class Statistic(models.Model):
+    user = models.ForeignKey(User, related_name="connections", on_delete=models.CASCADE)
+    site = models.ForeignKey(Site, related_name="connections", on_delete=models.CASCADE)
+    page_views = models.PositiveIntegerField(default=0)
+    data_sent = models.PositiveIntegerField(default=0)
+    data_received = models.PositiveIntegerField(default=0)
+
+
+
