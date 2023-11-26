8cc75465e3e7cfc7cc8631fb480f2cc6f10a9c8e
CaCtUs1k <denischernish19012004@gmail.com>
2023-11-26 18:12:56 +0200

    split proxy_view and provide statistic

---
+++ main/utils.py
@@ -0,0 +1,62 @@
+from urllib.parse import urljoin
+
+import requests
+
+from main.models import Statistic
+
+
+def get_css(soup, url):
+    css_link = soup.find('link', {'rel': 'stylesheet'})
+
+    if css_link:
+        css_url = urljoin(url, css_link['href'])
+        css_response = requests.get(css_url)
+
+        if css_response.status_code == 200:
+            css_content = css_response.text
+
+            style_tag = soup.new_tag('style')
+            style_tag.string = css_content
+            soup.head.append(style_tag)
+        else:
+            print(f"Failed to fetch CSS. Status code: {css_response.status_code}")
+
+
+def get_scripts(soup, url):
+    script_tags = soup.find_all('script', {'src': True})
+
+    for script_tag in script_tags:
+        script_src = script_tag['src']
+        script_url = urljoin(url, script_src)
+
+        script_response = requests.get(script_url)
+
+        if script_response.status_code == 200:
+            script_content = script_response.text
+            new_script_tag = soup.new_tag('script')
+            new_script_tag.string = script_content
+            script_tag.replace_with(new_script_tag)
+        else:
+            print(f"Failed to fetch script. Status code: {script_response.status_code}")
+
+
+def reformat_href(soup, proxy):
+    for a_tag in soup.find_all("a", href=True):
+        if "https://" not in a_tag["href"]:
+            a_tag["href"] = f"/{proxy.name}/{proxy.url}/{a_tag['href']}"
+    for form_tag in soup.find_all("form", action=True):
+        if "https://" not in form_tag["action"]:
+            form_tag["action"] = f"/{proxy.name}/{proxy.url}/{form_tag['action']}"
+
+
+def update_statistic(request, proxy, bytes_sent, bytes_received, page_views):
+    user = request.user
+    site = proxy
+
+    statistic, created = Statistic.objects.get_or_create(user=user, site=site)
+
+    statistic.page_views += page_views
+    statistic.data_sent += bytes_sent
+    statistic.data_received += bytes_received
+
+    statistic.save()
--- main/views.py
+++ main/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 
+from main import utils
 from main.models import Site, Statistic
 
 
@@ -16,6 +17,7 @@ class ListStatisticView(LoginRequiredMixin, generic.ListView):
     model = Statistic
     fields = "__all__"
     template_name = "vpn_service/statistic_list.html"
+    context_object_name = "statistics"
 
     def get_queryset(self):
         return Statistic.objects.filter(user=self.request.user)
@@ -47,41 +49,15 @@ def proxy_view(request, proxy_name, proxy_url):
 
     response = requests.get(proxy_url)
 
+    bytes_sent = len(request.body) if request.body else 0
+    bytes_received = len(response.content)
+
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    css_link = soup.find('link', {'rel': 'stylesheet'})
+    utils.get_css(soup, proxy_url)
+    utils.get_scripts(soup, proxy_url)
+    utils.reformat_href(soup, proxy)
 
-    if css_link:
-        css_url = urljoin(proxy_url, css_link['href'])
-        css_response = requests.get(css_url)
-
-        if css_response.status_code == 200:
-            css_content = css_response.text
-
-            style_tag = soup.new_tag('style')
-            style_tag.string = css_content
-            soup.head.append(style_tag)
-        else:
-            print(f"Failed to fetch CSS. Status code: {css_response.status_code}")
-
-    script_tags = soup.find_all('script', {'src': True})
-
-    for script_tag in script_tags:
-        script_src = script_tag['src']
-        script_url = urljoin(proxy_url, script_src)
-
-        script_response = requests.get(script_url)
-
-        if script_response.status_code == 200:
-            script_content = script_response.text
-            new_script_tag = soup.new_tag('script')
-            new_script_tag.string = script_content
-            script_tag.replace_with(new_script_tag)
-        else:
-            print(f"Failed to fetch script. Status code: {script_response.status_code}")
-
-    for a_tag in soup.find_all("a", href=True):
-        if "https://" not in a_tag["href"]:
-                a_tag["href"] = f"/{proxy_name}/{proxy.url}{a_tag['href']}"
+    utils.update_statistic(request, proxy, bytes_sent, bytes_received, page_views=1)
 
     return HttpResponse(str(soup), content_type=response.headers["content-type"])
