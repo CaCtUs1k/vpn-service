7fb1a8dfdcf7230c6108e56d63930c1c10c776bd
Denys Chernysh <78649918+CaCtUs1k@users.noreply.github.com>
2023-11-29 15:34:04 +0200

    Develop (#3)
    * initial core components
    
    * Provide models
    
    * Separate urls.py in main app
    
    * Provide base home page
    
    * install DjDT
    
    * Provide register and login
    
    * register models to admin
    
    * create sites proxy and login requirements
    
    * Create profile
    
    * Provide access to 1 url (vpn in process)
    
    * code refactor
    
    * __str__ methods to models
    
    * Totally separate user and program
    
    * Global HTML fix
    
    * Provide requirements
    
    * split proxy_view and provide statistic
    
    * Update and delete for site model
    
    * margin fixes
    
    * .env update
    
    * formatting using black
    
    * provide docker
    
    * README update
    
    * try to parse proxy with selenium

--- main/utils.py
+++ main/utils.py
@@ -1,58 +1,101 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from main.models import Statistic
 
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import os
 
-def get_css(soup, url):
+
+def download_page_with_selenium(url, folder_path, proxy):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    try:
+        driver.get(url)
+        wait = WebDriverWait(driver, 1)
+        wait.until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+        html_content = driver.page_source
+
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        os.makedirs(folder_path, exist_ok=True)
+
+        process_css_scripts(soup, url)
+
+        process_media_links(soup, url, folder_path, proxy)
+
+        reformat_href(soup, proxy)
+
+        reformat_static_img(soup, proxy)
+
+        with open(os.path.join(folder_path, 'index.html'), 'w', encoding='utf-8') as f:
+            f.write(str(soup))
+
+    finally:
+        driver.quit()
+
+    return soup
+
+
+def process_css_scripts(soup, url):
     css_link = soup.find("link", {"rel": "stylesheet"})
 
     if css_link:
         css_url = urljoin(url, css_link["href"])
-        css_response = requests.get(css_url)
+        css_content = requests.get(css_url).text
+        style_tag = soup.new_tag("style")
+        style_tag.string = css_content
+        soup.head.append(style_tag)
 
-        if css_response.status_code == 200:
-            css_content = css_response.text
 
-            style_tag = soup.new_tag("style")
-            style_tag.string = css_content
-            soup.head.append(style_tag)
+def process_media_links(soup, url, folder_path, proxy):
+    for element in soup.find_all(['img', 'link', 'script'], {'src': True}):
+        src = element['src']
+        if src.startswith(('http:', 'https:')):
+            src_url = src
         else:
-            print(
-                f"Failed to fetch CSS. Status code: {css_response.status_code}"
-            )
+            src_url = urljoin(url, src)
+        download_media(src_url, folder_path, proxy)
 
 
-def get_scripts(soup, url):
-    script_tags = soup.find_all("script", {"src": True})
+def download_media(url, base_folder, proxy):
+    response = requests.get(url, stream=True)
 
-    for script_tag in script_tags:
-        script_src = script_tag["src"]
-        script_url = urljoin(url, script_src)
-
-        script_response = requests.get(script_url)
-
-        if script_response.status_code == 200:
-            script_content = script_response.text
-            new_script_tag = soup.new_tag("script")
-            new_script_tag.string = script_content
-            script_tag.replace_with(new_script_tag)
+    if response.status_code == 200:
+        file_name = os.path.basename(urlparse(url).path)
+        if "/static/" not in url:
+            file_path = os.path.join(base_folder, file_name)
         else:
-            print(
-                f"Failed to fetch script. Status code: {script_response.status_code}"
-            )
+            file_path = f"static/{proxy.name}/" + url[url.find('/static/') + len("/static/"):]
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+
+
+def reformat_static_img(soup, proxy):
+    for img_tag in soup.find_all("img", src=True):
+        if "/static/" in img_tag["src"]:
+            img_tag["src"] = img_tag["src"].replace("/static/", f"/static/{proxy.name}/")
+        if "/media/" in img_tag["src"]:
+            img_tag["src"] = f"/media/{proxy.name}/" + img_tag["src"].split("/")[-1]
 
 
 def reformat_href(soup, proxy):
     for a_tag in soup.find_all("a", href=True):
-        if "https://" not in a_tag["href"]:
+        if not a_tag["href"].startswith(('http:', 'https:')):
             a_tag["href"] = f"/{proxy.name}/{proxy.url}/{a_tag['href']}"
     for form_tag in soup.find_all("form", action=True):
-        if "https://" not in form_tag["action"]:
-            form_tag[
-                "action"
-            ] = f"/{proxy.name}/{proxy.url}/{form_tag['action']}"
+        if not form_tag["action"].startswith(('http:', 'https:')):
+            form_tag["action"] = f"/{proxy.name}/{proxy.url}{form_tag['action']}"
 
 
 def update_statistic(request, proxy, bytes_sent, bytes_received, page_views):
